86da3d3fb7f76feaba6038e1b04ae1f9338ef57f
xfry <fry.andrade@gmail.com>
2020-12-15 14:33:49 -0500

    handling exeception

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -22,6 +22,22 @@ namespace Arrays
             PrintArray(array1d);
             Console.WriteLine("\n");
             PrintArray(array2d);
+
+            //Try catch examples
+            try
+            {
+                foreach(int item in array1d)
+                    System.Console.WriteLine("result: {0}\n", DivideItem(item));
+            }
+            catch (DivideByZeroException)
+            {
+                System.Console.WriteLine("There was an exception dividing by Cero");
+                throw;
+            }
+            finally
+            {
+                System.Console.WriteLine("te exception was handled and finished");
+            }
         }
         
         //Se crea una funcion que recibe un arreglo como parametros
@@ -36,6 +52,12 @@ namespace Arrays
         {
             foreach(int item in arr)
                 System.Console.WriteLine("Item: {0} ", item);
+        }
+
+        public static int DivideItem(int el)
+        {   
+            int b = 0;
+            return el/b;
         }
     }
 }

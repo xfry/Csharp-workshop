43d93fb3e63cc264a78c7f688d4b8a8c9d5a0f00
xfry <fry.andrade@gmail.com>
2021-01-01 15:26:15 -0500

    Added enum ActorTypes and CheckClass method

---
+++ OOP/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Actor Human = new Actor();
+            Human.Strength = 40;
+
+            Console.WriteLine("Hello World! {0}", Human.Strength);
+            Console.WriteLine("Runnin speed: {0}", Human.Run());
+            Console.WriteLine("move: {0}", Human.Constitution);
+            System.Console.WriteLine("Is Human?: {0}", Human.CheckClass(ActorType.Elfo));
+        }
+    }
+}

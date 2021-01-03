c03c8d2eb7b74bd8c3f1096b42dad4691cd06f72
xfry <fry.andrade@gmail.com>
2021-01-03 16:03:03 -0500

    added more comments to Boxing and Unboxing

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -6,6 +6,10 @@ namespace OOP
     {
         static void Main(string[] args)
         {
+            int a = 123;
+            Object b = a;   //boxing al valor de a
+            a = (int)b;     //unboxing al valor.
+
             //Usamos el value type Vec2, y le hacemos Boxing (Lea Struct.cs para entender.)
             Vec2 Vec = new Vec2(100,200);
             Vec2 VecCopy = Vec;

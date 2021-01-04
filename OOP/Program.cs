44f8f1e6acea5d94dae9d9b61e7041427e829dd8
xfry <fry.andrade@gmail.com>
2021-01-04 17:33:44 -0500

    adding a empty program file

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -6,30 +6,7 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            int a = 123;
-            Object b = a;   //boxing al valor de a
-            a = (int)b;     //unboxing al valor.
 
-            //Usamos el value type Vec2, y le hacemos Boxing (Lea Struct.cs para entender.)
-            Vec2 Vec = new Vec2(100,200);
-            Vec2 VecCopy = Vec; //Boxing implicito
-            
-            //Demostrando por que las Structs son Value Type
-            VecCopy.Y = 500;
-            Console.WriteLine($"Vec: {Vec}");
-            Console.WriteLine($"VecCopy: {VecCopy}");
-            Console.WriteLine($"Vec: after modified VecCopy: {Vec}");
-            ChangeAndDisplay(VecCopy);
-            Console.WriteLine($"VecCopy: after calling ChangeAndDisplay: {VecCopy}");
-            Vec = (Vec2)VecCopy; //Unboxing explicito
-            Console.WriteLine($"Vec - despues de Uboxing: {Vec}");
-        }
-
-        //Metodo que recibe un Structura por valor.
-        public static void ChangeAndDisplay (Vec2 Vec)
-        {
-            Vec.X = 300;
-            Console.WriteLine($"copy of VecCopy: {Vec}");
         }
     }
 }

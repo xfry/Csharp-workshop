f5f7c71fecca2aa71a87ce4267183410131f4d26
xfry <fry.andrade@gmail.com>
2021-01-04 17:54:40 -0500

    Added new Class with properties getter and setters

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -6,7 +6,21 @@ namespace OOP
     {
         static void Main(string[] args)
         {
+            Person Fredy = new Person(25, "Fredy Enrique");
+            Console.WriteLine("Peron Name: {0}", Fredy.Name);
+            Console.WriteLine("Person Age: {0}", Fredy.Age);
 
+
+            //Podemos inicializar las propiedades de una clase mediante le uso de Object Inicialization
+            var Mom = new Person()
+            {
+                Age = 56
+                //Name = "Yubenny", no puede ser usado aqui, porque el descriptor de la propiedad es private.
+            };
+
+            Mom.setName("Yubenny Andrade");
+            Console.WriteLine("Peron Name: {0}", Mom.Name);
+            Console.WriteLine("Person Age: {0}", Mom.Age);
         }
     }
 }
